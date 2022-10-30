3e807835a37e99137936562e03e7c27f8f39176d
Eric-Catarina <2020954200@teiacoltec.org>
2022-10-29 22:21:41 -0300

    fix: Client IDs changing

--- Server/server.js
+++ Server/server.js
@@ -3,24 +3,26 @@ import WebSocket, { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: 3000 });
 
 console.log("SERVER started")
-
+let CurrentClientID = 0
+let payloadToAllClients = {}
 
 
 wss.on('connection', function connection(ws) {
+  CurrentClientID ++
+  ws.send(JSON.stringify({"id": CurrentClientID.toString()}));
 
   ws.on('message', function message(data) {
-    let CurrentClientID = 0
-    //Sending a message to all clients
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        CurrentClientID ++
-        console.log(CurrentClientID)
-        client.send(JSON.stringify({"id": CurrentClientID.toString()}));
-      }
-    });
-
     console.log(JSON.parse(data))
   });
 
 });
+
+function SendPayloadToAllClients(payloadToAllClients){
+      //Send a message to all clients
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(payloadToAllClients));
+    }
+  });
+}
 
