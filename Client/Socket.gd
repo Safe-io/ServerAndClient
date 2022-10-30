7694d3058502a798470eab59bf9d54a68d6a167d
Eric-Catarina <2020954200@teiacoltec.org>
2022-10-30 11:27:51 -0300

    feat: Clients sharing positiion data

--- Client/Socket.gd
+++ Client/Socket.gd
@@ -34,12 +34,10 @@ func _connected(proto = ""):
 func _on_data():
 	packet =  JSON.parse(ws.get_peer(1).get_packet().get_string_from_utf8())
 	
-	if packet.result.has("id"):
-		myID = packet.result["id"]
-		print("Meu id:" + myID)
+	if packet.result.has("assignid"):
+		myID = packet.result["assignid"]
+	print(packet.result)
 	
-	
-		
 func _process(delta):
 	ws.poll()
 	playerPosition = (get_node("KinematicBody2D").position)
@@ -50,7 +48,8 @@ func _process(delta):
 		
 
 func send_player_position():
-	var payload = JSON.print({"msg" : playerPosition})
+	var payload = JSON.print({"playerposition" : playerPosition,
+								"id": myID})
 	ws.get_peer(1).put_packet((payload).to_utf8())
 	
 
--- Server/server.js
+++ Server/server.js
@@ -4,24 +4,25 @@ const wss = new WebSocketServer({ port: 3000 });
 
 console.log("SERVER started")
 let CurrentClientID = 0
+
 let payloadToAllClients = {}
 
 
 wss.on('connection', function connection(ws) {
   CurrentClientID ++
-  ws.send(JSON.stringify({"id": CurrentClientID.toString()}));
+  ws.send(JSON.stringify({"assignid": CurrentClientID.toString()}));
 
   ws.on('message', function message(data) {
+    SendPayloadToAllClients(data)
     console.log(JSON.parse(data))
   });
-
 });
 
 function SendPayloadToAllClients(payloadToAllClients){
       //Send a message to all clients
   wss.clients.forEach(function each(client) {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(payloadToAllClients));
+      client.send(payloadToAllClients);
     }
   });
 }
