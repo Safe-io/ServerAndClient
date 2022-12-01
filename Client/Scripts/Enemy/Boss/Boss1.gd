2bea1843b2edcac0c14665d640a205155e4ba544
Eric-Catarina <2020954200@teiacoltec.org>
2022-12-01 20:13:55 -0300

    refactor: instantiating cannons

--- Client/Scripts/Enemy/Boss/Boss1.gd
+++ Client/Scripts/Enemy/Boss/Boss1.gd
@@ -6,41 +6,36 @@ onready var MainNode = get_tree().root.get_child(0)
 var rotate_speed : float = 60.0
 var shoote_time_wait_time : float = 0.1
 var spawn_point_count : int = 6
-var bullet_speed : int = 12000
+var bullet_speed : int = 3000
 #bullet_speed não altera as formas que são geradas
 
 var max_health: float = 2000
 var health_points : float = 2000.0
 
-var Rotater 
+var Rotator 
 onready var hp_bar = $HPBar
+var BossCannonScene = preload("res://Scenes/Boss/BossCannon.tscn")
 var bullet_scene = preload("res://Scenes/Bullet/Boss1Bullet.tscn")
 
 func _ready():
 	HitSound = $HitSound
-	Rotater = $Rotater
-	var step = 2 * PI / spawn_point_count
+	Rotator = $Rotator
 	
-	for i in range(spawn_point_count):
-		var spawn_point = Node2D.new()
-		var pos = Vector2(110, 0).rotated(step * i)
-		spawn_point.position = pos
-		spawn_point.rotation = pos.angle()
-		Rotater.add_child(spawn_point)
+	instantiate_cannons(spawn_point_count)
 	
 	$ShootTimer.wait_time = shoote_time_wait_time
 	$ShootTimer.start()
 		
 func _physics_process(delta):
-	var new_rotation = Rotater.rotation_degrees + rotate_speed * delta
-	$Rotater.rotation_degrees = fmod(new_rotation, 360)
+	var new_rotation = Rotator.rotation_degrees + rotate_speed * delta
+	$Rotator.rotation_degrees = fmod(new_rotation, 360)
 	$Label.text = String (health_points)
 
 func _on_ShootTimer_timeout() -> void:
-	for s in Rotater.get_children():
+	for s in Rotator.get_children():
 		var bullet = bullet_scene.instance()
 		bullet.speed = bullet_speed
-		get_tree().root.add_child(bullet)
+		MainNode.add_child(bullet)
 		bullet.position = s.global_position
 		bullet.rotation = s.global_rotation
 	
@@ -61,6 +56,14 @@ func _on_Area2D_area_entered(area):
 		MainNode.increase_damage_points_dealed_in_the_frame()	
 	hp_bar.value = (health_points/max_health) * 100
 
-
 func _on_ReverseRotation_timeout():
 	rotate_speed = rotate_speed * -1
+
+func instantiate_cannons(number_of_cannons: int):
+	var step = 2 * PI / number_of_cannons
+	for index in range(number_of_cannons):
+		var current_boss_cannon = BossCannonScene.instance()
+		var current_boss_cannon_spawn_position = Vector2(110, 0).rotated(step * index)
+		current_boss_cannon.position = current_boss_cannon_spawn_position
+		current_boss_cannon.rotation = current_boss_cannon_spawn_position.angle()
+		Rotator.add_child(current_boss_cannon)
