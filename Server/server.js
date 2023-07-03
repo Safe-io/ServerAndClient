8853ac3d4c9c19e7c31aab18d5776e17f9b3cf78
Eric-Catarina <orment.cp@gmail.com>
2023-07-02 21:49:05 -0300

    refactor: server refactor

--- Server/server.js
+++ Server/server.js
@@ -4,137 +4,125 @@ import WebSocket, { WebSocketServer } from 'ws';
 import { createEnemy } from './enemy/enemy.js';
 import { handlePlayerHits } from './player/player.js';
 
-const wss = new WebSocketServer({ host: 'localhost', port: 3000 });
+// Constants
+const HOST = 'localhost';
+const PORT = 3000;
+const CLIENT_DISCONNECTED = 404;
+const PING_INTERVAL = 30000;
+const UPDATE_RATE = 1000 / 60;
 
-console.log("SERVER started")
+// Server setup
+const wss = new WebSocketServer({ host: HOST, port: PORT });
+console.log("SERVER started");
 
-let playerModel = {
-  pos: [0,0],
-  motion: [0,0],
-  r:0
+// Game state
+let GameState = {
+  enemies: {1 : createEnemy(2000)},
+  players: {}
+};
+
+// Track client IDs
+let currentClientID = 0;
+let availableIds = [];
+
+// Create a new player
+function createPlayer(id) {
+  return {
+    id,
+    pos: getRandomPosition(0, 1000),
+    motion: [0, 0],
+    r: 0
+  };
 }
-let CurrentClientID = 0
 
+// Handle a client connection
+function handleConnection(ws) {
+  ws.on('error', console.error);
+  ws.on('pong', heartbeat);
+  ws.on('close', handleDisconnection.bind(null, ws));
+  ws.on('message', handleMessage.bind(null, ws));
+  
+  ws.isAlive = true;
+  ws.id = assignClientID();
+  ws.send(JSON.stringify({"assignid": ws.id}));
+
+  GameState.players[ws.id] = createPlayer(ws.id);
+  ws.send(JSON.stringify({"assignposition": GameState.players[ws.id].pos}));
+}
+
+// Keep the connection alive
 function heartbeat() {
   this.isAlive = true;
 }
 
-let GameState = {
-  enemies : {1 : createEnemy(2000)},
-  players : {},
-}
-
-let availableIds = []
-
-const CLIENT_DISCONNECTED = 404
-
-let clientHasConected = (ws) => {
-  ws.on('error', console.error);
-
-  ws.isAlive = true;
+// Handle a client disconnection
+function handleDisconnection(ws) {
+  GameState.players[ws.id] = {"err": CLIENT_DISCONNECTED};
   delete GameState.players[ws.id];
-  AssignClientID(ws)
-  GameState.players[ws.id] = {...playerModel}
-  AssignClientRandomPosition(ws)
-  ws.on('pong', heartbeat);
-  ws.on('close', function clientHasDisconnected() {
-    GameState.players[ws.id] = {"err": CLIENT_DISCONNECTED}
-    delete GameState.players[ws.id]
-    availableIds.push(ws.id)
-    console.log(`Client with ID ${ws.id} has disconnected!`)
-    return
-  })
-  ws.on('message', function message(data) {
-    setTimeout(() => {
-      let dataObject = JSON.parse(data)
-      console.log("Received: " , dataObject)
-
-      if(typeof(data) === "object"){
-        if(!GameState.players[ws.id] || GameState.players[ws.id] === {"err": CLIENT_DISCONNECTED}) return
-      }
-      if(dataObject.type == "movement"){
-        console.log(GameState)
-        GameState.players[ws.id].pos = applyMovement( GameState.players[ws.id].pos, dataObject.motion)
-        delete GameState.players[ws.id].motion
-      }
-    }, 200);
-  });
-
-
+  availableIds.push(ws.id);
+  console.log(`Client with ID ${ws.id} has disconnected!`);
 }
 
-wss.on('connection', clientHasConected);
+// Handle incoming messages
+function handleMessage(ws, data) {
+  setTimeout(() => {
+    const dataObject = JSON.parse(data);
+    console.log("Received: " , dataObject);
 
-// sendPayloadToAllClients 60 seconds per second
-setInterval(() => {
-  if ( wss.clients.size > 0 ){
-    sendPayloadToAllClients(JSON.stringify(GameState));
-    console.log("Sent: " , GameState)
-  }
-}, 1000 / 60);
-
-
-
-
-function sendPayloadToAllClients(payloadToAllClients){
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(payloadToAllClients);
+    if (dataObject.type == "movement" && GameState.players[ws.id]) {
+      GameState.players[ws.id].pos = applyMovement(GameState.players[ws.id].pos, dataObject.motion);
     }
-  });
+  }, 200);
 }
 
-function AssignClientID(ws){
-  if(availableIds.length > 0){
-    ws.id = availableIds.pop()
+// Send state to all clients at a regular interval
+setInterval(sendPayloadToAllClients, UPDATE_RATE);
+
+// Send current game state to all clients
+function sendPayloadToAllClients() {
+  if (wss.clients.size > 0) {
+    const payload = JSON.stringify(GameState);
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload);
+      }
+    });
+    console.log("Sent: " , GameState);
   }
-  else{
-    CurrentClientID++
-    ws.id = CurrentClientID
-  }
-  console.log(`Client id: ${CurrentClientID} has connected!`)
-  ws.send(JSON.stringify({"assignid": CurrentClientID}));
 }
 
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
+// Assign a unique ID to each client
+function assignClientID() {
+  return availableIds.length > 0 ? availableIds.pop() : ++currentClientID;
+}
+
+// Ping clients regularly to keep connection alive
+const interval = setInterval(pingClients, PING_INTERVAL);
+
+// Ping all clients
+function pingClients() {
+  wss.clients.forEach(ws => {
     if (ws.isAlive === false) return ws.terminate();
 
     ws.isAlive = false;
     ws.ping();
     console.log("ping sent");
   });
-}, 30000);
+}
 
-wss.on('close', function close() {
-  clearInterval(interval);
-});
-
+// Apply motion to a position
 function applyMovement(position, motion) {
-  let playerVelocity = []
-  let playerPosition = position
-  let playerSpeed = 2
-  // Apply the input to the player's velocity
-  playerVelocity[0] = motion[0] * playerSpeed;
-  playerVelocity[1] = motion[1] * playerSpeed;
-
-  // Update the player's position based on their velocity
-  playerPosition[0] += playerVelocity[0];
-  playerPosition[1] += playerVelocity[1];
-
-  return playerPosition;
+  const playerSpeed = 2;
+  const playerVelocity = motion.map(m => m * playerSpeed);
+  return position.map((pos, index) => pos + playerVelocity[index]);
 }
 
+// Get a random position
 function getRandomPosition(min, max) {
-  let x = Math.random() * (max - min) + min;
-  let y = Math.random() * (max - min) + min;
-  return [x, y];
+  const randomCoordinate = () => Math.random() * (max - min) + min;
+  return [randomCoordinate(), randomCoordinate()];
 }
 
-function AssignClientRandomPosition(ws){
-  let randomPosition = getRandomPosition(0, 1000)
-  GameState.players[ws.id].pos = [0,0]
-  GameState.players[ws.id].pos = randomPosition
-  ws.send(JSON.stringify({"assignposition": randomPosition}));
-}
-
+// Start the server
+wss.on('connection', handleConnection);
+wss.on('close', () => clearInterval(interval));
